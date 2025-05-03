f45fe36d81786c226ff975c61cd2b3af02c09ea4
Pramananda Sarkar <pramanandasarkar02@gmail.com>
2025-05-03 19:16:16 +0600

    api gateway package created

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 
+var (
+	API_GATEWAY_PORT = ":8080"
+)
+
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+
+	log.Printf("Starting server on http://localhost%s", API_GATEWAY_PORT)
+	r.Run(API_GATEWAY_PORT)
+
+
+}
