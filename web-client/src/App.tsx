c03bcbba49044ae9e207c03187d508fdadd18a50
Pramananda Sarkar <pramanandasarkar02@gmail.com>
2025-07-26 19:02:13 +0600

    fearture/frontend: friend list and chat created

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Games from './pages/Games';
 import PlayerHome from './pages/PlayerHome';
 import Profile from './pages/Profile';
+import Friends from './pages/Friends';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/home" element={<PlayerHome />} />
       <Route path="/games" element={<Games />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/friends" element={<Friends />} />
 
     </Routes>
   );
--- web-client/src/components/Header.tsx
+++ web-client/src/components/Header.tsx
@@ -1,25 +1,35 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { FaUserCircle } from 'react-icons/fa'
+import { FaUserCircle, FaUserFriends } from 'react-icons/fa'
 
 type Props = {}
 
 const Header = (props: Props) => {
     const navigate = useNavigate()
-    
+
     return (
         <header className="bg-gray-800 text-white p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-2xl font-bold cursor-pointer" onClick={() => navigate('/')}>
                     Game Server
                 </div>
-                <div 
-                    className="flex items-center space-x-2 cursor-pointer hover:text-gray-300 transition"
-                    onClick={() => navigate('/profile')}
-                >
-                    <FaUserCircle size={24} />
-                    <span>Profile</span>
+                <div className='flex space-x-4'>
+                    <div
+                        className="flex items-center space-x-2 cursor-pointer hover:text-gray-300 transition"
+                        onClick={() => navigate('/friends')}
+                    >
+                        <FaUserFriends size={24} />
+                        <span>Friends</span>
+                    </div>
+                    <div
+                        className="flex items-center space-x-2 cursor-pointer hover:text-gray-300 transition"
+                        onClick={() => navigate('/profile')}
+                    >
+                        <FaUserCircle size={24} />
+                        <span>Profile</span>
+                    </div>
                 </div>
+
             </div>
         </header>
     )
