f449743ed6002bf3d98dd0e3b595d9b14a2fccc2
pramanandasarkar02 <pramanandasarkar02@gmail.com>
2025-10-30 09:08:19 +0600

    code refactoring for snake game

--- server/internal/api/snakeGameAPI.go
+++ server/internal/api/snakeGameAPI.go
@@ -10,14 +10,17 @@ import (
 
 
 func SnakeGameDataRoutes(router *gin.Engine) {
-
+	// create snake service to communicate each other
 	snakeService := snake.NewSnakeService()
 	
+	// create snake handler to handle snake game meta data
 	snakeGameHandler := handler.NewSnakeHandler(snakeService)
 
+	// get game specific meta data
 	router.GET("/api/game/snake/meta-data",snakeGameHandler.MetaData)
+	// get player match specific metadata
 	router.GET("/api/game/snake/meta-data/:playerId", snakeGameHandler.GameMetaData)
-
+	// main game logic end point 
 	router.GET("/ws", snake.WsHandler)
 
 	
